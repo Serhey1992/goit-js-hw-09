bfb085b5040272132911640a314c1cc87dee7da1
Serhey1992 <110050291+Serhey1992@users.noreply.github.com>
2022-12-21 19:17:45 +0000

    Deploying to gh-pages from @ Serhey1992/goit-js-hw-09@d5eaf748c5643a0387f680e56a0505425e37b1aa 🚀

---
+++ 01-color-switcher.647783a1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){intervalId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),2e3),e.setAttribute("disabled","disabled")})),n.addEventListener("click",(function(){e.removeAttribute("disabled"),clearInterval(intervalId)}))}();
+//# sourceMappingURL=01-color-switcher.647783a1.js.map
