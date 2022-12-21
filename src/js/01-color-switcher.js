d5eaf748c5643a0387f680e56a0505425e37b1aa
Serhey1992 <makss92i@gmail.com>
2022-12-21 21:17:13 +0200

    first done+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+
+startBtn.addEventListener('click', onClickStartBtn);
+stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 2000);
+    startBtn.setAttribute('disabled', 'disabled');
+};
+
+function onClickStopBtn() {
+    startBtn.removeAttribute('disabled');
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
