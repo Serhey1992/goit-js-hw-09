fae0671945da2d382a05eb045b622212d43791e6
Serhey1992 <makss92i@gmail.com>
2022-12-23 14:55:48 +0200

    second +/-

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,79 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const inputEl = document.querySelector('#datetime-picker');
+const startBtnEl = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let deadLine = 0;
+startBtnEl.disabled = 'disabled';
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    console.dir(selectedDates[0]);
+      if (selectedDates[0] < new Date()) {
+        alert('Please choose a date in the future')
+        startBtnEl.disabled = 'disabled';
+      } else {
+        startBtnEl.removeAttribute('disabled');
+        deadLine = selectedDates[0].getTime();
+      }
   },
 };
 
 flatpickr("#datetime-picker", options);
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    let currentTime = new Date().getTime();
+    const timeDifference = deadLine - currentTime;
+    const time = convertMs(timeDifference);
+    timeInterface(time);
+    
+    if (timeDifference <= 0) {
+      clearInterval(intervalId);
+    }
+
+  }, 1000)
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function timeInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+};
+
+
+function addZero(value) {
+  return value.padStart(2, '0');
+}
