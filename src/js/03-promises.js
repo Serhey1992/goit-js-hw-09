e0923e47975327845c48365b0b19a25d9234570e
Serhey1992 <makss92i@gmail.com>
2022-12-25 12:05:06 +0200

    third +/-

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const formEl = document.querySelector('.form');
+const delayEL = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submut', onFromSubmit);
+
+
+function onFromSubmit(e) {
+  e.preventDefault();
+  let delayPromise = Number(delayEL.value);
+
+  for (let i = 0; i < amountEl.value; i += 1) {
+    createPromise(i, delayPromise).then(onSuccess).catch(onError);
+    delayPromise += Number(stepEl.value);
+  }
+}
+
+
+
+
+function onSuccess(result) {
+  console.log(result);
+}
+
+function onError(error) {
+  console.log(error);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay} ms`);
+      }
+      reject(`Rejected promise ${position} in ${delay} ms`);
+    }, delay);
+  });
 }
