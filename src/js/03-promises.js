5c96ca17a8bf8d7fe2cf43f63dacf0ecd27a953b
Serhey1992 <makss92i@gmail.com>
2022-12-25 21:42:31 +0200

    third +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const formEl = document.querySelector('.form');
 const delayEL = document.querySelector('[name="delay"]');
 const stepEl = document.querySelector('[name="step"]');
@@ -11,28 +13,29 @@ function onFromSubmit(evt) {
   let delayPromise = Number(delayEL.value);
 
   for (let i = 0; i < amountEl.value; i += 1) {
-    createPromise(i, delayPromise).then(onSuccess).catch(onError);
+    createPromise(i, delayPromise).then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
     delayPromise += Number(stepEl.value);
   }
 }
 
-
-function onSuccess(result) {
-  console.log(result);
-}
-
-function onError(error) {
-  console.log(error);
-}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`Fulfilled promise ${position} in ${delay} ms`);
+        resolve({position, delay});
       }
-      reject(`Rejected promise ${position} in ${delay} ms`);
+      reject({position, delay});
     }, delay);
   });
 }
